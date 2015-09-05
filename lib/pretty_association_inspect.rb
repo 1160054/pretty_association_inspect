d41552f539cc73c1a7dc249c9eb090944172828d
root <root@tk2-205-12326.vs.sakura.ne.jp>
2015-09-05 23:31:44 +0900

    init

--- lib/pretty_association_inspect.rb
+++ lib/pretty_association_inspect.rb
@@ -1,12 +1,88 @@
 # -*- coding: utf-8 -*-
-require "pretty_association_inspect/version"
-
-# -*- coding: utf-8 -*-
 # モデルに関連情報を見やすく表示するメソッドを定義します。
 module PrettyAssociationInspect
-
   extend self
-  # 全てのモデルにメソッドを定義する
+
+  # 『関連を可愛く表示するメソッド』を定義する
+  def pretty_association_inspect_define(klass)
+    klass.class_eval do |model|
+      self.define_singleton_method(:to){
+        associations_hash = PrettyAssociationInspect.build_association_hash(model)
+        PrettyAssociationInspect.printed(klass, model, associations_hash)
+        return self.last
+      }
+
+      define_method(:to){
+        associations_hash = PrettyAssociationInspect.build_association_hash(model)
+        PrettyAssociationInspect.printed(klass, model, associations_hash)
+        return self
+      }
+    end
+  end
+
+  # アソシエーションをハッシュに変換
+  def build_association_hash(model)
+    model.reflect_on_all_associations.each_with_object({}) do |m, hash|
+      name = m.class.class_name.gsub("Reflection", "")
+      hash[name] ||= []
+      human_name = PrettyAssociationInspect.japanese_scripe(m.klass.model_name.human)
+      hash[name] << {association: m.name, human: human_name, model_name: m.active_record.name }
+    end
+  end
+
+  # バリューを整形
+  def value_convert(k, v, klass)
+    klass.class_eval {
+      is_e  = Object.const_defined?(:Enumerize) && first.send(k).kind_of?(Enumerize::Value)
+      return "#{v} #{first.send(k).text} #{send(k).values} #{send(k).values.map(&:text)}" if is_e
+      return v.strftime("%y年%m月%d日 %H:%M") if v.respond_to?(:strftime)
+      return columns_hash[k.to_s].type if v.blank?
+    }
+  end
+
+  # 表示
+  def printed(klass, model, associations_hash)
+    pretty_associations_array = []
+    klass.class_eval{|klass|
+      klass.first.attributes.each{|k ,v|
+        column = "%20s" % k
+        value  = PrettyAssociationInspect.value_convert(k, v, klass)
+        jp_val = PrettyAssociationInspect.japanese_scripe(klass.human_attribute_name(k))
+        db_val = "%5s"  % v
+        pretty_associations_array <<  "#{column} #{jp_val} #{db_val}"
+      }
+    }
+    puts "-"*100;
+    puts klass.name + "#{japanese_scripe(klass.model_name.human)}"
+    puts "[クラスメソッド]"
+    base_pattern   = "(before|after|around)_(add|remove|restore)|_associated_records_for_|inherited"
+    extr_pattern   = "attribute_type_decorations|soft_de|_restore_callback|indexed_|_by_resource"
+    delete_pattern = Regexp.new( [ base_pattern, extr_pattern ].join('|') )
+    puts (model.methods(false) - model.instance_methods).delete_if{|name|
+      delete_pattern.match(name) }.sort.join(', ')
+    puts "[インスタンスメソッド]"
+    puts (model.instance_methods(false) - model.superclass.instance_methods).delete_if{|name|
+      delete_pattern.match(name) }.sort.join(', ')
+    puts "[バリデーション]"
+    puts model.validators.map{|m|
+      m.class.name.gsub(/Active|Record|Validations|Model|Validator/,"")
+        .concat(": #{m.attributes.join(', ')} #{m.options}") }.sort
+    puts "[アソシエーション]"
+    associations_hash.each do |k, v|
+      puts "%10s"%k + "  " + v.map{ |m| [m[:association], m[:human]].join }.join(', ')
+    end
+    puts "[詳細]"
+    puts pretty_associations_array
+    puts "-"*100
+  end
+
+  # 日本語だけ抽出
+  def japanese_scripe(str)
+    japanese = Regexp.new(/[亜-熙ぁ-んァ-ヶ]/)
+    "(#{str})" if japanese =~ str
+  end
+
+    # 全てのモデルにメソッドを定義する
   def all_models_define
     model_names_array = load_all_models
     model_names_array.each do |model_name|
@@ -22,47 +98,6 @@ module PrettyAssociationInspect
     return ActiveRecord::Base.subclasses.map(&:name)
   end
 
-  # 表示
-  def printed(class_name, model, associations_hash)
-    puts "-"*100;
-    puts class_name
-    puts "[クラスメソッド]"
-    delete_pattern = Regexp.new("_(associated|after|before|for|soft)_")
-    puts (model.methods(false) - model.instance_methods).delete_if{|name| delete_pattern.match(name) }.sort.join(', ')
-    puts "[インスタンスメソッド]"
-    puts (model.instance_methods(false) - model.superclass.instance_methods).delete_if{|name| delete_pattern.match(name) }.sort.join(', ')
-    puts "[バリデーション]"
-    puts model.validators.map{|m| m.class.name.gsub(/ActiveRecord::Validations::|Validator/,"").concat(": #{m.attributes.join(', ')}") }.sort
-    puts "[アソシエーション]"
-    associations_hash.each { |k, v| puts "%10s  #{v.join(', ')}" % k }
-    puts "-"*100
-  end
-
-  # 『関連を可愛く表示するメソッド』を定義する
-  def pretty_association_inspect_define(klass)
-    klass.class_eval do |model|
-      self.define_singleton_method(:to){
-        associations_hash = model.reflect_on_all_associations.each_with_object({}) do |m, hash|
-          name = m.class.class_name.gsub("Reflection", "")
-          hash[name] ||= []
-          hash[name] << m.name
-        end
-        PrettyAssociationInspect.printed(klass.name, model, associations_hash)
-        return self.last || self
-      }
-    end
-
-    klass.class_eval do |model|
-      define_method(:to){
-        associations_hash = model.reflect_on_all_associations.each_with_object({}) do |m, hash|
-          hash[m.class.class_name] ||= []
-          hash[m.class.class_name] << m.name
-        end
-        PrettyAssociationInspect.printed(klass.name, model, associations_hash)
-        return self
-      }
-    end
-  end
 end
 
 PrettyAssociationInspect.all_models_define
